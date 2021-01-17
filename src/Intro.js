172dd9d9f564c52fff49e891627578f00c6cb61d
Andrew Dauphinais <idollabschicago@gmail.com>
2021-01-17 16:47:03 -0600

    Experiment with slider and animated stars background. Will probably not use either

---
+++ src/Intro.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <>
+      <div id='stars'></div>
+      <div id='stars2'></div>
+      <div id='stars3'></div>
+      <div id='title'>Hello</div>
+    </>
+  );
+};
+
+export default Intro;
