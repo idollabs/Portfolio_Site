f36fb453f373766630d91b41c0beb29e724e3b73
Andrew Dauphinais <idollabschicago@gmail.com>
2021-01-19 15:09:46 -0600

    Added grid component

---
+++ src/Grid.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Grid = () => {
+  return (
+    <section className='grid'>
+      <div className='portfolioItem'></div>
+      <div className='portfolioItem'></div>
+      <div className='portfolioItem'></div>
+      <div className='portfolioItem'></div>
+      <div className='portfolioItem'></div>
+      <div className='portfolioItem'></div>
+      <div className='portfolioItem'></div>
+      <div className='portfolioItem'></div>
+      <div className='portfolioItem'></div>
+      <div className='portfolioItem'></div>
+      <div className='portfolioItem'></div>
+      <div className='portfolioItem'></div>
+      <div className='portfolioItem'></div>
+      <div className='portfolioItem'></div>
+      <div className='portfolioItem'></div>
+    </section>
+  );
+};
+
+export default Grid;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-import './stars.css';
+import './index.css';
 import App from './App';
 import Intro from './Intro';
 import Slider from './Slider';
+import Grid from './Grid';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Intro />
+    <Grid />
   </React.StrictMode>,
   document.getElementById('root')
 );
